2d5ceb774fee38fd77cd9a5938df1effb5724732
Sawan Srivastava <sawan1210@gmail.com>
2025-06-08 20:00:26 -0700

    more clarity in comments

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Determining type of Mac
 os=$(uname -s)
 
-# Check for Mac type (intel or apple silicon)
+# Check for Mac type (intel or Apple Silicon)
 if [[ "$os" == "Darwin" ]]; then
   echo "Installing Homebew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
