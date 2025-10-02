e309295a16e674bbff00fc7f04162400c64ed54b
Sawan Srivastava <sawan1210@gmail.com>
2025-10-02 15:08:41 -0700

    add fish config
    Signed-off-by: Sawan Srivastava <sawan1210@gmail.com>

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set JAVA_HOME /home/sawan/.sdkman/candidates/java/24-tem
+set JAVA24_HOME /home/sawan/.sdkman/candidates/java/24-tem
+
+oh-my-posh init fish --config "/home/sawan/oh-my-posh/themes/space.omp.json" | source
